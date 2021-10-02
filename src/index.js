dfacf71402603ea3029c3e26921e79beb6c71ac7
Your Name <you@example.com>
2021-10-03 03:23:54 +0700

    commit boottrap

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 800;
 
+const route = require('./routes');
 // USE static folder 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(
+  express.urlencoded({
+  extended:true 
+  })
+);
 app.use(express.json());
 
 app.use(methodOverride('_method'));
@@ -24,24 +27,17 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-// console.log('PATH: ', path.join(__dirname, 'resources/views'))
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-app.get('/search', (req, res) => {
-  // console.log(req.query)
-  res.render('search');
-})
-app.post('/search', (req, res) => {
 
-  console.log(req.body.q);
-
-  res.render('search');
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+
+// Local host -- Hosting
+
+// Action -- Dispatcher --> Function handler
