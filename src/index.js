f713fd1ee6e0648d3dcd4f0ebca362ace18a1f00
Your Name <you@example.com>
2021-10-03 15:30:38 +0700

    prettier,lint-stage,husky

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const app = express();
 const port = 800;
 
 const route = require('./routes');
-// USE static folder 
-app.use(express.static(path.join(__dirname,'public')));
+// USE static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-  extended:true 
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -22,9 +22,12 @@ app.use(methodOverride('_method'));
 // HTTP logger
 // app.use(morgan('combined'))
 // Template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -32,11 +35,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
 // Local host -- Hosting
 
